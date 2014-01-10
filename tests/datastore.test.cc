bf517bbb91bb65485cbe0095941a59b78733a223
Lukasz Panek <lukasz.panek@nsn.com>
2014-01-10 11:03:03 +0100

    Add: testy dla modulu datastore

--- tests/datastore.test.cc
+++ tests/datastore.test.cc
@@ -11,31 +11,39 @@
 
 struct FakeFileFormat : public FileFormatInterface {
     FakeFileFormat()
+        : s(""), d("")
     {
     }
     std::string serialize(const TaskVec&)
     {
         return s;
     }
-    int deserialize(std::string&, TaskVec&)
+    int deserialize(std::string& image, TaskVec&)
     {
+        // Nie testuje tutaj metody fileformat->deserialize
+        // tylko interfejsy i obiekt typu DataStore.
+        // Zastepuje wiec metode deserialize ktora jes wywolywana z wnetrza load
+        // fakowa metoda ktora przypisze mi do zmiennej 'd' to co odczytaly
+        // mechanizmy DataStore.
+        d = image;
     }
     std::string s;
+    std::string d;
 };
 
 BOOST_AUTO_TEST_SUITE(Datastore_Test);
 
+const std::string dupajasia = "Dupa Jasia";
+const std::string filename = "test_dupajasia.db";
+TaskVec fakeVec;    // fakeowy kontener
 BOOST_AUTO_TEST_CASE(function_save)
 {
     DataStore<FakeFileFormat> saver;
     // Fakeujemy to co wyplowa serializer
-    std::string dupajasia = "Dupa Jasia";
     FakeFileFormat* const fff = dynamic_cast<FakeFileFormat*>(saver.getFFObj());
     fff->s = dupajasia;
 
     // zapis do pliku
-    TaskVec fakeVec;    // fakeowy kontener
-    const std::string filename = "test_dupajasia.db";
     bool ret = saver.save(filename, fakeVec);
     BOOST_CHECK_EQUAL(ret, true);
 
@@ -49,6 +57,37 @@ BOOST_AUTO_TEST_CASE(function_save)
     file.read(buf, dupajasia.size() + 1 + 10);
     BOOST_CHECK_EQUAL(dupajasia.compare(buf), 0);
     file.close();
+}
+
+BOOST_AUTO_TEST_CASE(function_load)
+{
+    // write some testing dato in to file
+    std::ofstream file;
+    file.open(filename, std::ostream::out);
+    BOOST_CHECK_EQUAL(file.good(), true);
+    file.write(dupajasia.c_str(), dupajasia.size());
+    file.close();
+
+    // check is stdlib file read methods work...
+    std::ifstream infile;
+    infile.open(filename, std::istream::in);
+    char* buf = new char[100];
+    memset(buf, 0, 100);
+    infile.read(buf, dupajasia.size());
+    infile.close();
+    BOOST_REQUIRE_EQUAL(dupajasia.compare(buf), 0);
+
+    // if so, test our class loader/reader
+    DataStore<FakeFileFormat> loader;
+    FakeFileFormat* const fff = dynamic_cast<FakeFileFormat*>(loader.getFFObj());
+
+    // test is our mock value is properly initialized
+    BOOST_CHECK_EQUAL(fff->d.compare(""), 0);
+
+    loader.load(filename, fakeVec);
+
+    BOOST_TEST_MESSAGE(fff->d);
+    BOOST_CHECK_EQUAL(dupajasia.compare(fff->d), 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
